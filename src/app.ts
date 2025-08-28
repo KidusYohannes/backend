eb5e3bb236e2d759decf976b1ea77a02b02e255a
Kidus-SANEDIGITAL <kidusy@sdtech.email>
2025-08-28 14:27:18 +0300

    removing cors for now

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ const app = express();
 
 app.use(helmet());
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || '*', // Set to your frontend URL in production
+  origin: '*', // Set to your frontend URL in production process.env.CORS_ORIGIN || 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
